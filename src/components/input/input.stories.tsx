6a01ce82f1955945d720589f326d755b3b8160d3
kmsu44 <kmsu444@gmail.com>
2024-02-13 14:59:20 +0900

    [#11] feat: input컴포넌트 제작

---
+++ src/components/input/input.stories.tsx
@@ -0,0 +1,38 @@
+import Input from '@/components/input/input';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Input> = {
+  title: 'input',
+  component: Input,
+  argTypes: {
+    value: {
+      control: {
+        type: 'text',
+      },
+    },
+    type: {
+      control: 'radio',
+      options: ['text', 'textarea'],
+    },
+    placeholder: {
+      control: {
+        type: 'text',
+      },
+    },
+    className: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Input>;
+
+export const InputStory: Story = {
+  args: {
+    value: '',
+    type: 'text',
+  },
+};
