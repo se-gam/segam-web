0fb26429e5626cbd3d59e482eb840cbe34f4ac6c
kmsu44 <kmsu444@gmail.com>
2024-02-13 23:36:52 +0900

    [#13] feat: board 컴포넌트 생성

---
+++ src/components/board/board.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import Icons from '@/components/common/icons/icons';
+import useLink from '@/hooks/useLink';
+
+interface BoardProps {
+  title: string;
+  url: string;
+  children?: React.ReactNode;
+}
+
+export default function Board({ title, children, url }: BoardProps) {
+  const { navigateTo } = useLink();
+  return (
+    <section className="rounded-2xl bg-white px-3 py-5">
+      <div className="mb-4 flex items-center justify-between px-3">
+        <h2 className="f20 font-bold text-text_primary">{title}</h2>
+        <button
+          type="button"
+          aria-label={`go to ${title}`}
+          onClick={() => {
+            navigateTo(url);
+          }}
+        >
+          <Icons.ArrowRight className="w-4.5 h-4.5 fill-theme_tertiary" />
+        </button>
+      </div>
+      {children}
+    </section>
+  );
+}
