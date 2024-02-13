a2814b2785cb349fb20dd961d5f395c55736a209
kmsu44 <kmsu444@gmail.com>
2024-02-13 23:35:41 +0900

    [#13] feat: attendanceCard 컴포넌틑 생성

---
+++ src/components/cards/attendanceCard.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Icons from '@/components/common/icons/icons';
+import Tag from '@/components/common/tag/tag';
+import useLink from '@/hooks/useLink';
+
+interface AttendanceCardProps {
+  title: string;
+  iconName: string;
+}
+export default function AttendanceCard({
+  title,
+  iconName,
+}: AttendanceCardProps) {
+  const { navigateTo } = useLink();
+
+  return (
+    <button
+      type="button"
+      className="flex w-full items-center justify-between gap-4 rounded-md p-3 active:bg-gray-200"
+      onClick={() => {
+        navigateTo(`attendance/${title}`);
+      }}
+    >
+      <div className="flex items-center gap-4">
+        <div className="rounded-md bg-icons_bg p-0.5">
+          <Icons.ImageIcon name={iconName} width={36} height={36} />
+        </div>
+        <p className="f16 font-bold text-text_primary">{title}</p>
+      </div>
+      <Tag size="sm" variant="done" label="1개" />
+    </button>
+  );
+}
