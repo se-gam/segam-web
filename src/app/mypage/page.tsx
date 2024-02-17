c2d2987602153f8bf533b5bf2f4c8260f2c8aef8
kmsu44 <kmsu444@gmail.com>
2024-02-17 20:31:30 +0900

    [#19] chore: mypage디자인 변경

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -30,9 +30,9 @@ const auth = [
 ];
 export default function MyPage() {
   return (
-    <main className="container h-full overflow-auto bg-white">
+    <main className="container h-full overflow-auto bg-app_bg">
       <header className="mb-4.5 px-4 py-2.5">
-        <h1 className="f20 font-bold text-text_primary">이러닝</h1>
+        <h1 className="f20 font-bold text-text_primary">내 정보</h1>
       </header>
       <section className="mb-3.5 flex w-full flex-col items-center justify-center">
         <div className="mb-3 h-24 w-24 rounded-full bg-app_bg p-5">
@@ -41,14 +41,15 @@ export default function MyPage() {
         <h2 className="f20 font-bold text-text_primary">바이너리형</h2>
         <p className="f16 font-medium text-text_secondary">컴퓨터 공학과</p>
       </section>
-      <section className="mb-3.5">
-        <h1 className="f20 mb-2 px-4 font-bold text-text_primary">설정</h1>
-        <div className="space-y-1 px-4">
+      <div className="h-2.5 w-full bg-app_bg" />
+      <h1 className="f20 mb-2 px-4 font-bold text-text_primary">설정</h1>
+      <section className="bg-white py-2">
+        <div className="space-y-1 px-2">
           {settings.map((setting) => (
             <button
               key={setting.label}
               type="button"
-              className="flex w-full items-center justify-between rounded bg-white px-2 py-4 transition-transform active:scale-[0.98] active:bg-gray-300"
+              className="flex w-full items-center justify-between rounded bg-white px-2 py-4 transition-transform active:scale-[0.98] active:bg-app_bg"
             >
               <span className="f16 font-medium text-text_primary ">{setting.label}</span>
               <Icons.ArrowRight className="fill-theme_tertiary" width="1rem" height="1rem" />
@@ -56,13 +57,14 @@ export default function MyPage() {
           ))}
         </div>
       </section>
+      <div className="h-2.5 w-full bg-app_bg" />
       <section>
-        <div className="space-y-1 px-4">
+        <div className="space-y-1 bg-white px-2 py-2">
           {auth.map((fn) => (
             <button
               key={fn.label}
               type="button"
-              className="flex w-full items-center justify-between rounded bg-white px-2 py-4 transition-transform active:scale-[0.98] active:bg-gray-300"
+              className="flex w-full items-center justify-between rounded bg-white px-2 py-4 transition-transform active:scale-[0.98] active:bg-app_bg"
             >
               <span className="f16 font-medium text-text_primary">{fn.label}</span>
               <Icons.ArrowRight className="fill-theme_tertiary" width="1rem" height="1rem" />
