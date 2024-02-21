c4ef7fa7995123d53fc021e943a33ca535aae3de
kmsu44 <kmsu444@gmail.com>
2024-02-21 16:37:50 +0900

    [#23] feat: course API 연동

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -1,10 +1,10 @@
 'use server';
 
+import { updateCourseAttendance } from '@/lib/actions/attendance';
 import { AuthResponse } from '@/lib/definitions';
 import encryptPassword from '@/utils/encryptPassword';
 import fetchExtended from '@/utils/fetchExtended';
 import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 
 export default async function login(formData: FormData) {
   const studentId = formData.get('studentId');
@@ -21,14 +21,8 @@ export default async function login(formData: FormData) {
     .catch(() => {
       throw new Error('학번과 비밀번호를 확인하세요.');
     });
-  cookies().set('accessToken', accessToken, {
-    maxAge: 60 * 60 * 24 * 7,
-  });
-  cookies().set('refreshToken', refreshToken, {
-    maxAge: 60 * 60 * 24 * 168,
-  });
-  cookies().set('encrypted', encryptedPassword, {
-    maxAge: 60 * 60 * 24 * 168,
-  });
-  redirect('/dashboard');
+  cookies().set('accessToken', accessToken, { maxAge: 60 * 60 * 24 * 7 });
+  cookies().set('refreshToken', refreshToken, { maxAge: 60 * 60 * 24 * 168 });
+  cookies().set('encrypted', encryptedPassword, { maxAge: 60 * 60 * 24 * 168 });
+  await updateCourseAttendance();
 }
