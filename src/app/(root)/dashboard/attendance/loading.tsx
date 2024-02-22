d607485a845789a1466a763849a6457fe82e4e4e
kmsu44 <kmsu444@gmail.com>
2024-02-22 16:21:32 +0900

    [no-issue] remove: 중복 로딩 삭제

--- src/app/(root)/dashboard/attendance/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Spin } from 'antd';
-
-export default function Loading() {
-  return (
-    <div className="container flex h-dvh flex-col items-center justify-center">
-      <Spin />
-    </div>
-  );
-}
