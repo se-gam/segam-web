43e1deaaa9e875259841053e9ec239c02846704d
kmsu44 <kmsu444@gmail.com>
2024-02-22 14:34:31 +0900

    [no-issue] chore: 에러페이지 UI변경

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useEffect } from 'react';
+
 export default function Error({
   error,
   reset,
@@ -7,15 +9,19 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
   return (
     <main className="flex h-full flex-col items-center justify-center">
-      <h2 className="text-center">{error.message}</h2>
+      <h2 className="text-center">{error.digest}</h2>
+      <h2 className="text-center">서버 오류가 발생했습니다.</h2>
       <button
         type="button"
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
         onClick={() => reset()}
       >
-        Try again
+        돌아가기
       </button>
     </main>
   );
