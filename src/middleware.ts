f67c4b4cca228a638eb192819c88e75a987c731e
kmsu44 <kmsu444@gmail.com>
2024-02-22 14:33:56 +0900

    [no-issue] chore: 미들웨어 설정 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,17 +5,17 @@ export function middleware(request: NextRequest) {
   const hasPassword = request.cookies.has('encrypted');
   const hasAccessToken = request.cookies.has('accessToken');
   const hasRefreshToken = request.cookies.has('refreshToken');
+  const requestUrl = request.nextUrl.pathname;
   const isLoggedIn = hasPassword && hasAccessToken && hasRefreshToken;
-  if (request.nextUrl.pathname !== '/login' && !isLoggedIn) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
-  if (request.nextUrl.pathname === '/login' && isLoggedIn) {
+  if (isLoggedIn && (requestUrl === '/' || requestUrl === '/login')) {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
-
+  if (!isLoggedIn && requestUrl.includes('/dashboard')) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/login', '/dashboard/:path*'],
+  matcher: ['/login', '/dashboard/:path*', '/'],
 };
