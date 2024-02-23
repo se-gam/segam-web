a4bf569f1bf8fa90961fe59aac4d1452f926237b
kmsu44 <kmsu444@gmail.com>
2024-02-23 15:52:23 +0900

    [no-issue] chore: 버튼 컴포넌트 props변경

--- src/components/common/button/button.tsx
+++ src/components/common/button/button.tsx
@@ -9,39 +9,38 @@ interface ButtonProps {
   variant: 'default' | 'primary' | 'selected' | 'disabled';
   size: 'sm' | 'md' | 'lg' | 'full';
   className?: string;
+  type?: 'button' | 'submit';
   onClick?: () => void;
 }
 
-const ButtonVariants = cva(
-  'transition-opacity duration-200 active:opacity-80',
-  {
-    variants: {
-      variant: {
-        default: 'bg-button_default_bg text-text_primary',
-        primary: 'bg-theme_primary text-white',
-        selected: 'bg-button_default_bg text-theme_secondary',
-        disabled: ' bg-button_default_bg text-theme_tertiary',
-      },
-      size: {
-        sm: 'f12 rounded px-2 py-1.5 font-semibold',
-        md: 'f14 rounded px-3 py-1.5 font-semibold',
-        lg: 'f14 rounded px-4 py-1.5 font-semibold',
-        full: 'f16 h-12 w-full rounded-md font-bold',
-      },
+const ButtonVariants = cva('transition-opacity duration-200 active:opacity-80', {
+  variants: {
+    variant: {
+      default: 'bg-button_default_bg text-text_primary',
+      primary: 'bg-theme_primary text-white',
+      selected: 'bg-button_default_bg text-theme_secondary',
+      disabled: ' bg-button_default_bg text-theme_tertiary',
+    },
+    size: {
+      sm: 'f12 rounded px-2 py-1.5 font-semibold',
+      md: 'f14 rounded px-3 py-1.5 font-semibold',
+      lg: 'f14 rounded px-4 py-1.5 font-semibold',
+      full: 'f16 h-12 w-full rounded-md font-bold',
     },
   },
-);
+});
 
 export default function Button({
   label,
   variant,
   size,
   className = '',
+  type = 'button',
   onClick = () => {},
 }: ButtonProps) {
   return (
     <button
-      type="button"
+      type={type}
       onClick={onClick}
       className={cn(
         ButtonVariants({
