6ccc4b3955946de9ab6b986d58b1d853aeb421ea
kmsu44 <kmsu444@gmail.com>
2024-02-24 02:10:19 +0900

    [no-issue] fix: safari 폰트 이슈 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,32 @@ import { AntdRegistry } from '@ant-design/nextjs-registry';
 import type { Metadata, Viewport } from 'next';
 import localFont from 'next/font/local';
 import '@/app/global.css';
+import clsx from 'clsx';
 
 const pretendard = localFont({
-  src: './PretendardVariable.woff2',
+  src: [
+    {
+      path: './fonts/Pretendard-Regular.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Pretendard-Medium.woff',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Pretendard-SemiBold.woff',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Pretendard-Bold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-pretendard',
   display: 'swap',
 });
 // 줌 방지 코드
@@ -27,7 +50,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`container mx-auto h-dvh overflow-hidden ${pretendard.className}`}>
+      <body
+        className={clsx(pretendard.variable, 'container mx-auto h-dvh overflow-hidden font-sans')}
+      >
         <AntdRegistry>{children}</AntdRegistry>
       </body>
     </html>
