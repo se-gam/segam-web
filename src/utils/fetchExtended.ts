d8616bd94145306c4c307c7f7b9c56162dd3be3c
kmsu44 <kmsu444@gmail.com>
2024-02-27 01:26:28 +0900

    [no-issue] remove: retrunfetch 함수 제거

--- src/utils/fetchExtended.ts
+++ src/utils/fetchExtended.ts
@@ -1,6 +1,5 @@
 'use server';
 
-import { cookies } from 'next/headers';
 import returnFetch, { ReturnFetch } from 'return-fetch';
 import returnFetchJson from 'return-fetch-json';
 
@@ -9,7 +8,9 @@ const returnFetchThrowingErrorByStatusCode: ReturnFetch = (args) =>
     ...args,
     interceptors: {
       response: async (response) => {
-        if (response.status === 500) throw Error('서버 오류');
+        if (response.status === 500) {
+          throw new Error('서버에 문제가 발생했습니다.');
+        }
         if (response.status >= 400) {
           throw await response.text().then(Error);
         }
@@ -18,50 +19,10 @@ const returnFetchThrowingErrorByStatusCode: ReturnFetch = (args) =>
     },
   });
 
-const returnFetchRetry: ReturnFetch = (args) =>
-  returnFetch({
-    ...args,
-    interceptors: {
-      response: async (response, requestArgs, fetch) => {
-        if (response.status !== 401) return response;
-        if (cookies().has('accessToken')) cookies().delete('accessToken');
-        if (cookies().has('refreshToken')) cookies().delete('refreshToken');
-        if (!response.url.includes('/login')) return response;
-
-        const responseToRefreshCookie = await fetch(
-          `${process.env.NEXT_PUBLIC_BASE_URL}/v1/auth/refresh`,
-          {
-            method: 'POST',
-
-            body: JSON.stringify({
-              refreshToken: cookies().get('refreshToken')?.value,
-            }),
-          },
-        );
-        if (responseToRefreshCookie.status !== 201) {
-          throw Error('로그인이 만료되었습니다. 다시 로그인해주세요.');
-        }
-        const { accessToken, refreshToken } = await responseToRefreshCookie.json();
-        if (!accessToken || !refreshToken) throw new Error('오류');
-        if (accessToken && refreshToken) {
-          cookies().set('accessToken', accessToken, {
-            maxAge: 60 * 60 * 24 * 7,
-          });
-          cookies().set('refreshToken', refreshToken, {
-            maxAge: 60 * 60 * 24 * 168,
-          });
-        }
-        return fetch(...requestArgs);
-      },
-    },
-  });
-
 const fetchExtended = returnFetchJson({
   jsonParser: JSON.parse,
   fetch: returnFetchThrowingErrorByStatusCode({
-    fetch: returnFetchRetry({
-      baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
-    }),
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
   }),
 });
 export default fetchExtended;
