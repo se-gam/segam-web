73d5fdc1c744756fde2929e3421559a919af8b0f
Jinhyung Lee <luciancah@gmail.com>
2024-03-04 18:59:50 +0900

    [#69] feat: 스터디룸 시간 쿼리 프론트로 변경

--- src/components/studyroom/list/studyRoomSlotList.tsx
+++ src/components/studyroom/list/studyRoomSlotList.tsx
@@ -7,17 +7,43 @@ import useLink from '@/hooks/useLink';
 import { Studyroom } from '@/lib/definitions';
 import StudyRoomSlotItem from './studyRoomSlotItem';
 
+const filterStudyRooms = (studyRooms: Studyroom[], startsAt: number, endsAt: number) => {
+  const rooms = JSON.parse(JSON.stringify(studyRooms)); // 쿼리 좁히다가 다시 넓힐 것 대비해서 딥카피
+  const adjustedEndsAt = endsAt > startsAt ? endsAt - 1 : endsAt;
+
+  return rooms.filter((room: Studyroom) => {
+    const hasOpenSlotsInRange = room.slots.some(
+      (slot) =>
+        slot.startsAt >= startsAt &&
+        slot.startsAt <= adjustedEndsAt &&
+        !slot.isReserved &&
+        !slot.isClosed,
+    );
+
+    return hasOpenSlotsInRange;
+  });
+};
+
 interface StudyRoomSlotListProps {
   date: string;
   data: Studyroom[];
+  startsAt: number;
+  endsAt: number;
 }
 
-export default function StudyRoomSlotList({ date, data }: StudyRoomSlotListProps) {
+export default function StudyRoomSlotList({
+  date,
+  data,
+  startsAt,
+  endsAt,
+}: StudyRoomSlotListProps) {
   const { navigateTo } = useLink();
   const dateString = new Date(date).toISOString().split('T')[0];
+  const filteredData = filterStudyRooms(data, startsAt, endsAt);
+
   return (
     <div className="flex flex-col">
-      {data.map((studyroom) => (
+      {filteredData.map((studyroom: Studyroom) => (
         <div
           onClick={() => {
             navigateTo({
@@ -25,6 +51,7 @@ export default function StudyRoomSlotList({ date, data }: StudyRoomSlotListProps
               title: studyroom.name,
             });
           }}
+          key={studyroom.id}
         >
           <StudyRoomSlotItem key={studyroom.id} data={studyroom} />
         </div>
