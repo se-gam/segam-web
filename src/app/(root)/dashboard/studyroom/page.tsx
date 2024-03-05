f62238e29bc9c62dedbb8b9f10ea251551d815b6
Jinhyung Lee <luciancah@gmail.com>
2024-03-05 20:17:48 +0900

    [#75] feat: 스터디룸 컴포넌트 패딩 추가

--- src/app/(root)/dashboard/studyroom/page.tsx
+++ src/app/(root)/dashboard/studyroom/page.tsx
@@ -21,7 +21,7 @@ export default async function Page({
       <header className="mb-4 px-4 pt-2.5">
         <h1 className="f20 font-bold text-text_primary">스터디룸</h1>
       </header>
-      <section className="mb-5 space-y-2">
+      <section className="mb-5 space-y-2 px-4">
         <h1 className="f24 font-bold text-text_primary">내 예약 현황</h1>
         <Suspense fallback={<Loading size="small" />}>
           <ReservationView />
