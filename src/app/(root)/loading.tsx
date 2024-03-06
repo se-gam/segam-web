053af0860a303f694a33d00aecd86198d30d01a4
kmsu44 <kmsu444@gmail.com>
2024-03-07 01:37:37 +0900

    [#83] chore: 알파 테스트용 커밋

--- src/app/(root)/loading.tsx
+++ src/app/(root)/loading.tsx
@@ -2,7 +2,7 @@ import { Spin } from 'antd';
 
 export default function Loading() {
   return (
-    <div className="container flex h-screen flex-col items-center justify-center gap-3">
+    <div className="container flex h-dvh flex-col items-center justify-center gap-3">
       <p className="f20 font-bold text-text_primary">강의 정보를 업데이트 하고 있어요...</p>
       <Spin />
     </div>
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { Spin } from 'antd';
 
 export default function Loading() {
   return (
-    <div className="container flex h-screen flex-col items-center justify-center gap-3">
+    <div className="container flex h-dvh flex-col items-center justify-center gap-3">
       <Spin />
     </div>
   );
