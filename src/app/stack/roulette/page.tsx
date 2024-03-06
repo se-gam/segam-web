0dc957c654b5b2026888f5e878e9d1d35ca35185
kmsu44 <kmsu444@gmail.com>
2024-03-06 15:28:36 +0900

    [#83] chore: 룰렛페이지 스크롤 오류 해결

--- src/app/stack/roulette/page.tsx
+++ src/app/stack/roulette/page.tsx
@@ -7,14 +7,16 @@ export default async function RoulettePage() {
   return (
     <>
       <div className="absolute -z-10 h-full w-full bg-gradient-to-b from-roulette_bg to-white" />
-      <main className="container flex h-full w-full flex-col items-center overflow-x-hidden overflow-y-scroll bg-transparent">
+      <div className="flex h-full flex-col overflow-hidden">
         <StackHeader bgClass="bg-transparent" />
-        <div className="f28 pt-[2.625rem] font-bold text-text_primary">점심 뭐 먹을까?</div>
-        <div className=" f16 p-1 font-medium text-text_secondary">
-          세종대 주변 58개의 맛집중에서 골랐어요
-        </div>
-        <MenuRoulette menus={data.restaurants} />
-      </main>
+        <main className="container flex w-full flex-col items-center overflow-x-hidden overflow-y-scroll bg-transparent">
+          <div className="f28 pt-[2.625rem] font-bold text-text_primary">점심 뭐 먹을까?</div>
+          <div className=" f16 p-1 font-medium text-text_secondary">
+            세종대 주변 58개의 맛집중에서 골랐어요
+          </div>
+          <MenuRoulette menus={data.restaurants} />
+        </main>
+      </div>
     </>
   );
 }
