72c5030b25ee3ef6bc849fcd9a6d3e8177950a97
kmsu44 <kmsu444@gmail.com>
2024-08-25 18:28:53 +0900

    [#160] style: 고전독서 대시보드 UI 변경

--- src/app/(root)/dashboard/classic/page.tsx
+++ src/app/(root)/dashboard/classic/page.tsx
@@ -7,10 +7,12 @@ import GoteukReservationCard from '@/components/classic/goteukReservationCard';
 export default async function ClassicPage() {
   return (
     <main className="page container overflow-auto pb-8">
-      <header className="mb-4 px-4 pt-2.5">
+      <header className="px-4 pt-2.5">
         <h1 className="f20 font-bold text-text_primary">고전독서인증</h1>
       </header>
-      <section className="mb-5 space-y-2 px-4">
+      <GoteukReservationCard />
+      <div className="h-1.5 w-full bg-button_default_bg" />
+      <section className="space-y-2 px-4 py-5">
         <h1 className="f24 font-bold text-text_primary">예약 현황</h1>
         <Suspense fallback={<Loading size="small" />}>
           <GoteukReservationList />
@@ -21,7 +23,6 @@ export default async function ClassicPage() {
         <h2 className="f20 mb-[18px] font-bold text-text_primary">인증현황</h2>
         <GoteukStatusList />
       </section>
-      <GoteukReservationCard />
     </main>
   );
 }
--- src/components/classic/goteukReservationCard.tsx
+++ src/components/classic/goteukReservationCard.tsx
@@ -6,24 +6,19 @@ import useLink from '@/hooks/useLink';
 export default function GoteukReservationCard() {
   const { navigateTo } = useLink();
   return (
-    <div className="w-full px-4">
-      <button
-        className="w-full rounded-2xl bg-button_default_bg px-4 py-5"
-        onClick={() => {
-          navigateTo({ page: 'roulette' });
-        }}
-      >
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-2">
-            <Icons.ImageIcon name="classic_west" width={36} height={36} />
-            <div className="flex flex-col gap-1 text-left">
-              <p className="f14 font-medium text-text_secondary">고전특강을 듣기 싫다면?</p>
-              <p className="f18 font-bold text-text_primary">독서인증시험 예약하기</p>
-            </div>
-          </div>
-          <Icons.ArrowRight className="fill-theme_tertiary" width="1rem" height="1rem" />
+    <button
+      className="w-full rounded-2xl p-4"
+      onClick={() => {
+        navigateTo({ page: 'roulette' });
+      }}
+    >
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col text-left">
+          <p className="f14 font-medium text-text_secondary">고전특강을 듣기 싫다면?</p>
+          <p className="f16 font-bold text-text_primary">독서인증시험 예약하기</p>
         </div>
-      </button>
-    </div>
+        <Icons.ArrowRight className="fill-theme_tertiary" width="1rem" height="1rem" />
+      </div>
+    </button>
   );
 }
