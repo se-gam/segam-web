b259e8591bd11a1c5d9fec2df07daf75fd992306
kmsu44 <kmsu444@gmail.com>
2024-08-29 04:00:39 +0900

    [#160] chore: discord훅 로컬에서는 제외

--- src/lib/actions/discord.ts
+++ src/lib/actions/discord.ts
@@ -1,4 +1,8 @@
 export default async function postMessageToDiscord(title: string, message: string) {
+  if (process.env.NODE_ENV === 'development') {
+    console.log('Local 환경에서는 Discord로 메세지를 보낼 수 없습니다.');
+    return;
+  }
   const DISCORD_URL = process.env.DISCORD_URL as string;
   await fetch(DISCORD_URL, {
     method: 'POST',
