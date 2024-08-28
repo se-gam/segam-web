0128f8d8c4741f33150dbcdf80a043dd9895ff50
MINSU KIM <kmsu444@gmail.com>
2024-08-28 15:31:30 +0900

    Fix/drawer error 167 (#171)
    * [#167] fix: drawer컴포넌트 -> portal방식으로 변경
    
    * [#167] chore: build오류 해결
    
    * [#167] style: 기존 스타일 통일

--- src/app/(root)/dashboard/studyroom/page.tsx
+++ src/app/(root)/dashboard/studyroom/page.tsx
@@ -3,19 +3,15 @@ import StudyRoomView from '@/components/studyroom/studyRoomView';
 
 export default async function Page() {
   return (
-    <div className="pb-0">
-      <div className="flex h-full flex-col overflow-hidden">
-        <main className="flex h-full flex-col overflow-hidden">
-          <header className="mb-4 px-4 pt-2.5">
-            <h1 className="f20 font-bold text-text_primary">스터디룸</h1>
-          </header>
-          <section className="mb-5 space-y-2 px-4">
-            <h1 className="f24 font-bold text-text_primary">내 예약 현황</h1>
-            <ReservationView />
-          </section>
-          <StudyRoomView />
-        </main>
-      </div>
-    </div>
+    <main className="flex h-full flex-col overflow-hidden">
+      <header className="mb-4 px-4 pt-2.5">
+        <h1 className="f20 font-bold text-text_primary">스터디룸</h1>
+      </header>
+      <section className="mb-5 space-y-2 px-4">
+        <h1 className="f24 font-bold text-text_primary">내 예약 현황</h1>
+        <ReservationView />
+      </section>
+      <StudyRoomView />
+    </main>
   );
 }
