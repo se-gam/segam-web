d26050cf499fc48bfc70680c9bb8a3ebdc100bbe
SungChul Hong <hsc6263@gmail.com>
2024-08-31 15:06:00 +0900

    [#164] fix: calendar props 타입 정의

--- src/components/classic/gtCalender.tsx
+++ src/components/classic/gtCalender.tsx
@@ -1,62 +1,25 @@
 'use client';
 
 import { Calendar, ConfigProvider, Select } from 'antd';
-import { HeaderRender } from 'antd/es/calendar/generateCalendar';
-import { createStyles } from 'antd-style';
 import locale from 'antd/locale/ko_KR';
 import dayjs, { Dayjs } from 'dayjs';
-
 import 'dayjs/locale/ko';
 import { useState } from 'react';
+
+import Icons from '@/components/common/icons/icons';
 import cn from '@/utils/cn';
-import Icons from '../common/icons/icons';
+import { CalendarProps } from 'antd/lib';
 
 dayjs.locale('ko');
 
-const useStyles = createStyles(({ css }) => ({
-  calender: css`
-    .ant-picker-panel {
-      border: none !important;
-    }
-    .ant-picker-content thead tr th {
-      font-size: 12px !important;
-      font-weight: 400 !important;
-      font-style: normal !important;
-      color: #979799 !important;
-      font-color: #979799 !important;
-    }
-    .ant-picker-content tbody tr td {
-      vertical-align: middle !important;
-      text-align: -webkit-center !important;
-    }
-    .ant-picker-content tbody tr td div {
-      width: 42px !important;
-      height: 42px !important;
-    }
-    .ant-picker-cell-disabled::before {
-      background-color: unset !important;
-    }
-    .ant-picker-cell {
-      padding: 0px !important;
-      font-size: 20px;
-    }
-    .ant-typography {
-      margin-bottom: 0px;
-    }
-    .ant-picker-cell-disabled {
-      background-color: none !important;
-    }
-  `,
-}));
-
-const headerRender: HeaderRender<Dayjs> = ({ value, onChange }) => {
+const headerRender: CalendarProps<Dayjs>['headerRender'] = ({ value, onChange }) => {
   const start = 0;
   const end = 12;
   const monthOptions = [];
 
   let current = value.clone();
   const months = [];
-  for (let i = 0; i < 12; i += 1) {
+  for (let i = start; i < end; i += 1) {
     current = current.month(i);
     months.push(current.format('MMMM'));
   }
@@ -111,18 +74,20 @@ const headerRender: HeaderRender<Dayjs> = ({ value, onChange }) => {
 };
 
 export default function GtCalender() {
-  const { styles } = useStyles();
   const [selectedDate, setSelectedDate] = useState(dayjs());
   const [currentMonth, setCurrentMonth] = useState(dayjs());
-  const cellRender = (currentDate: Dayjs, info: { type: 'date' | 'month' | 'year' }) => {
+  const cellRender: CalendarProps<Dayjs>['fullCellRender'] = (
+    date: Dayjs,
+    info: CellRenderInfo<Dayjs>,
+  ) => {
     if (info.type === 'date') {
       const isCurrentMonth =
-        currentDate.year() === currentMonth.year() && currentDate.month() === currentMonth.month();
+        date.year() === currentMonth.year() && date.month() === currentMonth.month();
       if (!isCurrentMonth) {
         return null;
       }
 
-      const isSelected = currentDate.isSame(selectedDate, 'day');
+      const isSelected = date.isSame(selectedDate, 'day');
 
       return (
         <div
@@ -131,7 +96,7 @@ export default function GtCalender() {
             'text-white': isSelected,
           })}
         >
-          {currentDate.date()}
+          {date.date()}
         </div>
       );
     }
@@ -150,8 +115,6 @@ export default function GtCalender() {
   return (
     <ConfigProvider locale={locale}>
       <Calendar
-        className={styles.calender}
-        // @ts-ignore
         fullCellRender={cellRender}
         fullscreen={false}
         headerRender={headerRender}
