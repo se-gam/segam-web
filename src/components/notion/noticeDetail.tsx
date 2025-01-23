3782091cdadf2b15087dc0a79cfecb2e41d86e08
seunghui-park <ridia1233@naver.com>
2025-01-24 01:39:08 +0900

    [#274] refactor: 공지사항 조회/상세 페이지 UI TailwindCSS 테마 적용

--- src/components/notion/noticeDetail.tsx
+++ src/components/notion/noticeDetail.tsx
@@ -11,11 +11,11 @@ export default function NoticeDetail({ notice }: NoticeDetailProps) {
     return (
       <div className="safe-area-bottom flex h-screen flex-col overflow-hidden bg-white">
         <header className="px-4">
-          <h1 className="text-[20px] font-bold text-[#4E5968]">{notice.title}</h1>
-          <time className="block mt-2 text-[16px] text-[#979799]">{dateDotFormatter(notice.createdAt)}</time>
+          <h1 className="text-[20px] font-bold text-text_primary">{notice.title}</h1>
+          <time className="block mt-2 text-[16px] text-text_secondary">{dateDotFormatter(notice.createdAt)}</time>
         </header>
         <main className="mt-4 flex-1 px-4 py-2 overflow-auto">
-          <p className="ext-[16px] text-[#4E5968] leading-6 whitespace-pre-wrap">{notice.content}</p>
+          <p className="ext-[16px] text-text_primary leading-6 whitespace-pre-wrap">{notice.content}</p>
         </main>
       </div>
     );
--- src/components/notion/noticeList.tsx
+++ src/components/notion/noticeList.tsx
@@ -42,10 +42,10 @@ export default function NoticeList() {
           href={`/stack/notion/${notice.id}`}
           className="flex flex-col justify-center h-[78px] px-4 py-[16px] hover:bg-gray-50 transition-colors cursor-pointer"
         >
-          <h2 className="text-[16px] font-semibold leading-[24px] text-[#4E5968] truncate">
+          <h2 className="text-[16px] font-semibold leading-[24px] text-text_primary truncate">
             {notice.title}
           </h2>
-          <time className="text-[14px] font-medium leading-[20px] text-[#979799]">
+          <time className="text-[14px] font-medium leading-[20px] text-text_secondary">
             {dateDotFormatter(notice.createdAt)}
           </time>
         </Link>
