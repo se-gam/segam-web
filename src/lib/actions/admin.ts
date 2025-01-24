431eb711be5ebc1a7ec4fc3033bb3667f652555b
InGwonKang <tnqkr3494@naver.com>
2025-01-24 16:06:43 +0900

    [#276] fix: url 값 변경 및 절대경로 수정

--- src/lib/actions/admin.ts
+++ src/lib/actions/admin.ts
@@ -3,8 +3,8 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { revalidateTag } from 'next/cache';
-import { fetchExtended } from '../../utils/fetchExtended';
-import { Notice } from '../definitions';
+import { fetchExtended } from '@/utils/fetchExtended';
+import { Notice } from '@/lib/definitions';
 
 const ADMIN_PASSWORD = '1234'; // 비밀번호 설정
 
@@ -27,21 +27,18 @@ export async function login(prevState: any, formData: FormData) {
 }
 
 export async function getNotices() {
-  const { body: notices } = await fetchExtended<Notice[]>(
-    'http://dev.api.segam.org:3000/v1/notice',
-    {
-      cache: 'force-cache',
-      next: {
-        tags: ['notices'],
-      },
+  const { body: notices } = await fetchExtended<Notice[]>('/v1/notice', {
+    cache: 'force-cache',
+    next: {
+      tags: ['notices'],
     },
-  );
+  });
 
   return notices;
 }
 
 export async function handleDelete(id: number) {
-  await fetchExtended(`http://dev.api.segam.org:3000/v1/notice/${id}`, {
+  await fetchExtended(`/v1/notice/${id}`, {
     method: 'DELETE',
   });
   revalidateTag('notices');
