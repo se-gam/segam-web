223a4500bd565985fa2806086474dd6eba2c3c33
kmsu44 <kmsu444@gmail.com>
2025-03-11 23:23:50 +0900

    [no-issue] refactor: scroll area

--- src/app/stack/notion/[id]/page.tsx
+++ src/app/stack/notion/[id]/page.tsx
@@ -3,19 +3,19 @@ import { Notice } from '@/lib/definitions';
 import { getNoticeDetail } from '@/lib/actions/notice';
 import NoticeDetail from '@/components/notion/noticeDetail';
 
-export default async function DetailPage({params}:{params:{id:number}}){
-    const notice : Notice|null = await getNoticeDetail(params.id)
+export default async function DetailPage({ params }: { params: { id: number } }) {
+  const notice: Notice | null = await getNoticeDetail(params.id);
 
-    if (!notice) {
-        throw new Error('공지사항을 불러올 수 없습니다.');
-    }
+  if (!notice) {
+    throw new Error('공지사항을 불러올 수 없습니다.');
+  }
 
-    return(
-        <div className = "container h-full flex flex-col overflow-hidden">
-            <StackHeader title = {notice.title}/>
-            <div className = "mt-8 h-full overflow-auto">
-                <NoticeDetail notice={notice}/>
-            </div>
-        </div>
-    );
+  return (
+    <div className="container flex h-full flex-col overflow-hidden">
+      <StackHeader title={notice.title} />
+      <div className="h-full overflow-auto">
+        <NoticeDetail notice={notice} />
+      </div>
+    </div>
+  );
 }
--- src/components/notion/noticeDetail.tsx
+++ src/components/notion/noticeDetail.tsx
@@ -1,22 +1,24 @@
-'use client'
+'use client';
 
 import { Notice } from '@/lib/definitions';
 import { dateDotFormatter } from '@/utils/format';
 
-interface NoticeDetailProps{
-    notice:Notice;
+interface NoticeDetailProps {
+  notice: Notice;
 }
 
 export default function NoticeDetail({ notice }: NoticeDetailProps) {
-    return (
-      <div className="safe-area-bottom flex h-screen flex-col overflow-hidden bg-white">
-        <header className="px-4">
-          <h1 className="f20 font-bold text-text_primary">{notice.title}</h1>
-          <time className="block mt-2 f16 text-text_secondary">{dateDotFormatter(notice.createdAt)}</time>
-        </header>
-        <main className="mt-1 flex-1 px-4 py-2 overflow-auto">
-          <p className="f16 text-text_primary leading-6 whitespace-pre-wrap">{notice.content}</p>
-        </main>
-      </div>
-    );
-  }
+  return (
+    <div className="safe-area-bottom mt-8 flex flex-col overflow-hidden bg-white">
+      <header className="px-4">
+        <h1 className="f20 font-bold text-text_primary">{notice.title}</h1>
+        <time className="f16 mt-2 block text-text_secondary">
+          {dateDotFormatter(notice.createdAt)}
+        </time>
+      </header>
+      <main className="mt-1 flex-1 overflow-auto px-4 py-2">
+        <p className="f16 whitespace-pre-wrap leading-6 text-text_primary">{notice.content}</p>
+      </main>
+    </div>
+  );
+}
